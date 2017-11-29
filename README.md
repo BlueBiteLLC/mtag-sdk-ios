53861c7314ba5d4675b2e45332e4328bdf74962f
Sam Krantz <sam@bluebite.com>
2017-11-29 15:33:19 -0500

    SDK-14: update indention create podspec update gitignore

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mtag-sdk-ios
-iOS SDK for the  mTag Platform (v2)
+iOS SDK for the  mTag Platform
+
+### Installation
+
+Simply clone this repo and add `mTag-SDK` as a framework for your project.  
--- mTag-SDK/InteractionDelegate.swift
+++ mTag-SDK/InteractionDelegate.swift
@@ -10,18 +10,18 @@ import Foundation
 
 /**
  Delegate handles responses, both successful and errors, from the mTag-SDK.
-*/
+ */
 public protocol BlueBiteInteractionDelegate {
-    /**
-     Called when an Interaction has been successfully registered with the BlueBite API.
-     - Parameter withResults result: Dictionary containing relevant information provided in the Interaction response.
-    */
-    func interactionDataWasReceived(withResults result: [String: Any])
-    /**
-     Called when an Interaction has failed to be registered with the BlueBite API.
+  /**
+   Called when an Interaction has been successfully registered with the BlueBite API.
+   - Parameter withResults result: Dictionary containing relevant information provided in the Interaction response.
+   */
+  func interactionDataWasReceived(withResults result: [String: Any])
+  /**
+   Called when an Interaction has failed to be registered with the BlueBite API.
 
-     This could be caused by a general API failure, an invalid URL, or a general Alamofire failure.
-     - Parameter _ error: Error message.
-    */
-    func didFailToReceiveInteractionData(_ error: String)
+   This could be caused by a general API failure, an invalid URL, or a general Alamofire failure.
+   - Parameter _ error: Error message.
+   */
+  func didFailToReceiveInteractionData(_ error: String)
 }
